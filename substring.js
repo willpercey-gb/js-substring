df01909e7ba5a0b27726c5c7407f6169c78bfb58
Lewis Raggett <lewisraggett17@gmail.com>
2020-07-21 10:25:30 +0100

    Added function

--- substring.js
+++ substring.js
@@ -1,5 +1,16 @@
 const substring = (document, start, end) => {
-    
+    let start_pos = document.indexOf(start);
+    let end_pos = document.indexOf(end);
+    if (start_pos === -1){
+        start_pos = 0;
+    }
+    else{
+        start_pos += start.length;
+    }
+    if (end_pos === -1){
+        end_pos = 0;
+    }
+    return document.substring(start_pos, end_pos);
 };
 
 export default {substring};
