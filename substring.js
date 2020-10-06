fd2b42874c0f33d035b733c53ccd74b75b4f100e
Will Percey <willpercey@googlemail.com>
2020-10-06 13:11:37 +0100

    allow bool values

--- substring.js
+++ substring.js
@@ -1,13 +1,22 @@
 export const substring = (document, start, end) => {
-    let start_pos = document.indexOf(start);
-    let end_pos = document.indexOf(end);
-    if (start_pos === -1){
+    let start_pos, end_pos;
+    document = String(document);
+    if (!start) {
         start_pos = 0;
+    } else {
+        start_pos = document.indexOf(start);
     }
-    else{
+    if (end) {
+        end_pos = document.indexOf(end);
+    } else {
+        end_pos = document.length;
+    }
+    if (start_pos === -1) {
+        start_pos = 0;
+    } else {
         start_pos += start.length;
     }
-    if (end_pos === -1){
+    if (end_pos === -1) {
         end_pos = 0;
     }
     return document.substring(start_pos, end_pos);
